7c66d1a17677c34899d5d4496a5357602e2ffee0
James Farr <james.farr@jdplc.com>
2024-05-03 14:38:52 +0100

    car data added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
+import { reactive } from 'vue';
+import carData from "./data/cars.json";
 
+const cars = reactive(carData);
 </script>
 
 <template>
-Test
+<ul>
+    <li v-for="car in cars" :key="car.id">{{ car.model }}</li>
+</ul>
 </template>
 
 <style scoped>
