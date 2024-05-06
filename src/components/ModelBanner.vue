6145552ccb52163ae8fe58b6472f9d874ab3f47c
James Farr <james.farr@jdplc.com>
2024-05-06 17:34:36 +0100

    Background image & overlay added to model component

--- src/components/ModelBanner.vue
+++ src/components/ModelBanner.vue
@@ -1,44 +1,81 @@
 <template>
-    <section style="background-image: url(&quot;https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Model-Y-Desktop-Global.png&quot;);height: 100vh;background-size: cover;width: auto;position: relative;background-position: center;">
+    <section class="background-banner" :style="{ backgroundImage: backgroundImg }">
         <div class="content-container">
-            <div class="title">
+            <div class="content-title">
                 <h1>Model Y</h1>
                 <h3>PCH from £399/mo*</h3>
             </div>
-            <div class="options-container">
+            <div class="content-body">
+                <div class="options-container">
                 <button class="btn btn-primary" type="button">Button</button>
                 <button class="btn btn-primary" type="button">Button</button>
+            </div>
+            <div class="test-drive-cta-container">
+                <a href="#">Schedule a Test Drive</a>
+            </div>
             </div>
         </div>
     </section>
 </template>
 
 <script setup>
+import { computed } from 'vue';
 
+
+const backgroundImg = computed(() => {
+    const isMobile = window.innerWidth < 768;
+
+    return isMobile ? 'url(https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Model-Y-Mobile-Global.jpg)' : 'url(https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Model-Y-Desktop-Global.png)';
+});
 </script>
 
 <style scoped>
+.background-banner {
+    height: 100vh;
+    background-size: cover;
+    width: auto;
+    position: relative;
+    background-position: center;
+}
 .content-container {
     position: absolute;
     display: flex;
     flex-direction: column;
     height: 100%;
     width: 100%;
-    justify-content: center;
+    justify-content: space-evenly;
     align-items: center;
-    gap: 55%;
+    gap: 45%;
+    background: rgba(0, 0, 0, 0.015);
 }
-.title {
+.content-title {
     text-align: center;
+    padding-top: 35px;
+}
+.content-title h3 {
+    font-size: 20px;
+    font-weight: normal;
 }
 .options-container {
     display: flex;
     gap: 15px;
     width: 100%;
-    justify-content: center;
+    justify-content: space-between;
 }
 .options-container button {
     width: 225px;
     height: auto;
+}
+.content-body {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+.test-drive-cta-container {
+    text-align: center;
+}
+.test-drive-cta-container a {
+    font-size: 12px;
+    color: #fff;
 }
 </style>
